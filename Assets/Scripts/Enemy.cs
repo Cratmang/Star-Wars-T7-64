607e257f5761e9d49045e453ec28493eea229d04
Connor Gilhuly, or Cratmang <cratmang@gmail.com>
2021-05-08 22:00:29 -0400

    Added Battle Droid enemies, which currently do nothing except Conga their way into Vault 641, then die. It was good for me that I had code for a tower defence game that I could reuse. (RIP, Defence Against Maniacal Nymphs. You never even made it off the ground.)

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,215 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Enemy : MonoBehaviour {
+
+    protected SpriteRenderer spriteRenderer;
+
+    public bool safe;
+
+    public int level;
+    public float health;
+    public float maxHealth;
+    public int[] bounty;
+    protected GameManager gm;
+    public List<Transform> waypoints;
+    public List<int> safeWaypoints;
+    protected int currentWaypoint = 0;
+    public float lastWaypointSwitchTime;
+    public float speed = 1.0f;
+    public bool moving;
+    public bool mounting;
+
+    public bool boosted;
+    public float boostMultiplier;
+
+    public bool cursed;
+    protected float curseTime;
+    protected float timeCursed;
+
+    public bool inked;
+    protected float inkTime;
+    protected float timeInked;
+
+    protected Vector3 startPosition;
+    protected Vector3 endPosition;
+
+    protected float pathLength;
+    public float totalTimeForPath;
+    public float currentTimeOnPath;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    public float distanceToGoal()
+    {
+        float distance = 0;
+        if (!mounting)
+        {
+            distance += Vector3.Distance(gameObject.transform.position, waypoints[currentWaypoint + 1].transform.position);
+            for (int i = currentWaypoint + 1; i < waypoints.Count - 1; i++)
+            {
+                Vector3 startPosition = waypoints[i].transform.position;
+                Vector3 endPosition = waypoints[i + 1].transform.position;
+                distance += Vector3.Distance(startPosition, endPosition);
+            }
+        }
+        return distance;
+    }
+
+    //public void Initiate() {
+        //gm = game;
+    //    lastWaypointSwitchTime = Time.time;
+        //waypoints = pw;
+    //}
+
+    /*public void Initiate(GameObject[] pw, List<int> sw, GameManager game)
+    {
+        gm = game;
+        lastWaypointSwitchTime = Time.time;
+        waypoints = pw;
+        safeWaypoints = sw;
+        startPosition = waypoints[currentWaypoint].transform.position;
+        endPosition = waypoints[currentWaypoint + 1].transform.position;
+        Vector2 vectorToTarget = waypoints[currentWaypoint + 1].transform.position - transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - 90;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        health = maxHealth[level];
+        moving = true;
+
+    }*/
+
+    public void Initiate(List<Transform> pw, GameManager gm, int startPoint)
+    {
+        this.gm = gm;
+        lastWaypointSwitchTime = Time.time;
+        waypoints = pw;
+        currentWaypoint = startPoint;
+        startPosition = waypoints[currentWaypoint].transform.position;
+        endPosition = waypoints[currentWaypoint + 1].transform.position;
+        Vector2 vectorToTarget = waypoints[currentWaypoint + 1].transform.position - transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - 90;
+        //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        health = maxHealth;
+        moving = true;
+    }
+
+
+    void Update()
+    {
+        
+        /*float step;
+        if (inked)
+        {
+            step = speed / 2.0F;
+        } else
+        {
+            step = speed;
+        }*/
+
+        //Movement
+        startPosition = waypoints[currentWaypoint].transform.position;
+        endPosition = waypoints[currentWaypoint + 1].transform.position;
+
+        pathLength = Vector3.Distance(startPosition, endPosition);
+        totalTimeForPath = pathLength / speed;//step;
+        currentTimeOnPath = Time.time - lastWaypointSwitchTime;
+        transform.position = Vector3.Lerp(startPosition, endPosition, currentTimeOnPath / totalTimeForPath);
+
+        ///Vector3 vectorToTarget = waypoints[currentWaypoint + 1].transform.position - transform.position;
+        //float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - 90;
+        //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), 0.1F);
+        //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        if (gameObject.transform.position.Equals(endPosition))
+        {
+            if (currentWaypoint < waypoints.Count - 2)
+            {
+                currentWaypoint++;
+                lastWaypointSwitchTime = Time.time;
+
+                //Add check if the next Waypoint is in the next room. If yes, teleport to the next room.
+                if (!waypoints[currentWaypoint+1]) {
+                    currentWaypoint += 2;
+                    transform.position = waypoints[currentWaypoint].transform.position;
+                }
+
+            }
+            else {//Crap! They've reached the end!
+                //gameManager.EnemyReachedGoal(bounty[level]);
+                Destroy(gameObject);
+            }
+        }
+
+        //Rotate/Point towards next waypoint
+        //1
+        //Vector3 newStartPosition = waypoints[currentWaypoint].transform.position;
+        //Vector3 newEndPosition = waypoints[currentWaypoint + 1].transform.position;
+        //Vector3 newDirection = (newEndPosition - newStartPosition);
+        //2
+        //float x = newDirection.x;
+        //float y = newDirection.y;
+        //float rotationAngle = Mathf.Atan2(y, x) * 180 / Mathf.PI;
+        //3
+        //GameObject sprite = (GameObject) gameObject.transform.FindChild("Sprite").gameObject;
+        //sprite.transform.rotation = Quaternion.AngleAxis(rotationAngle, Vector3.forward);
+
+
+        
+    }
+
+    protected void OnTriggerEnter2D(Collider2D other)
+    {
+        /*if (other.tag.Equals("Projectile"))
+        {
+            Projectile projectile = other.gameObject.GetComponent<Projectile>();
+            float damageTaken = projectile.damage;
+            if (cursed)
+            {
+                damageTaken *= 2;
+            }
+            health -= damageTaken;
+            projectile.HitTarget();
+        }
+        if (other.tag.Equals("CurseProjectile"))
+        {
+            Projectile projectile = other.gameObject.GetComponent<Projectile>();
+            cursed = true;
+            timeCursed = Time.time;
+            curseTime = projectile.damage;
+            projectile.HitTarget();
+        }
+        if (other.tag.Equals("Bomb"))
+        {
+            Bomb bomb = other.gameObject.GetComponent<Bomb>();
+            /*int damageTaken = bomb.damage;
+            if (cursed)
+            {
+                damageTaken *= 2;
+            }
+            health -= damageTaken;*
+            bomb.Detonate();
+        }
+        if (other.tag.Equals("Blast") && !other.name.Equals("EnemyBlastCircle(Clone)"))
+        {
+            Projectile projectile = other.gameObject.GetComponent<Projectile>();
+            float damageTaken = projectile.damage;
+            if (cursed)
+            {
+                damageTaken *= 2;
+            }
+            health -= damageTaken;
+        }
+        if (other.tag.Equals("InkSplat"))
+        {
+            InkSplat inkSplat = other.gameObject.GetComponent<InkSplat>();
+            inked = true;
+            timeInked = Time.time;
+            inkTime = inkSplat.damage;
+        }*/
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Enemy Paths
+    // Basically, the waypoints for every enemy path. These are meant to be mix-matched-stitched together to create a single path through the base.
+    
+    public List<Transform> enemyHangarPath;
+    public List<Transform> enemyVentPath; //Enemies should be able to SPAWN from the vent, but they need to go back to the Hangar to escape.
+    
+    //public List<Transform> enemyVault0PathHangar; // As there are three vaults, the enemy will branch one of three ways upon reaching the Atrium.
+    public List<Transform> enemyVault1PathHangar;
+    public List<Transform> enemyVault2PathHangar;
+
+    public List<Transform> enemyMinePath;
+    public List<Transform> enemyVault0PathMine;
+    public List<Transform> enemyVault1PathMine;
+    public List<Transform> enemyVault2PathMine;
+
+    public List<Transform> enemyVault0Path;
+    public List<Transform> enemyVault1Path;
+    public List<Transform> enemyVault2Path;
+
+    public List<Transform> enemyEscapePath0;
+    public List<Transform> enemyEscapePath1;
+    public List<Transform> enemyEscapePath2;
+    public List<Transform> enemyEscapePathToTheChopper; //The final rush through the halls!
+
+
+    //Spawn Timer
+    // Spawn enemies over time. Gonna replace this with a Wave-based system.
+    public GameObject battleDroid;
+    public float spawnTime;
+    float timer = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //TO-DO: Set up Wave spawning system
+
+        timer += Time.deltaTime;
+        if (timer >= spawnTime) {
+
+            GameObject newEn = Instantiate(battleDroid, enemyHangarPath[0].position, enemyHangarPath[0].rotation);
+            List<Transform> path = new List<Transform>();
+
+            //TO-DO: Set up path randomization. Choose between Vaults 0 - 2, start in the Hangar, the Vent, or the Mine.
+            path.AddRange(enemyHangarPath);
+            path.AddRange(enemyVault1PathHangar); 
+            path.AddRange(enemyVault1Path);
+            newEn.GetComponent<Enemy>().Initiate(path, this, 0);
+
+            timer = 0;
+        }
+    }
+}
