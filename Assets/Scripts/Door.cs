97bd119c63cf9667249958e5584031b9681c3d4f
Connor Gilhuly, or Cratmang <cratmang@gmail.com>
2021-05-19 14:45:40 -0400

    Atrium now has its own model, rather than being a collection of greyboxes and planes (no texture though). Added unique model and textures for doorways.

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -7,6 +7,8 @@ public class Door : MonoBehaviour
     public int nextLocationIndex;
     //The int that the PlayerManager script will use to determine the next part of the map.
     // Refer to PlayerManager or my design notes for a list of which index leads to which area.
+    // NOTE: Use of nextLocationIndex may no longer be needed, but I'm worried I might break something if I remove it, 
+    //       and considering we're less than a week from the submission deadline, I'd rather not risk it.
     public Room nextRoom;
     
 }
